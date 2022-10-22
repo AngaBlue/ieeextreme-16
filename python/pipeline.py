288c45f6f8281d8cea373a718cffc49a4447a5fa
AngaBlue <contact@anga.blue>
2022-10-22 15:38:01 +1100

    An attempt

--- python/pipeline.py
+++ python/pipeline.py
@@ -29,17 +29,20 @@ tests = get_number()
 
 for t in range(tests):
     # Create array with dimensions
-    n = get_number()
-    m = get_number()
-    d = get_number()
+    n = get_number() # lines per level
+    m = get_number() # characters per line
+    d = get_number() # levels
 
-    array = np.empty(shape=[n, m, d], dtype='S1')
+    array = np.empty(shape=[d, n, m], dtype='S1')
 
     # Populate array
-    for i in range(n):
-        for j in range(m):
+    for i in range(d):
+        for j in range(n):
             word = get_word()
-            for k in range(d):
+            while (len(word) < m):
+                word = get_word()
+                
+            for k in range(m):
                 array[i, j, k] = word[k]
 
     # Print array
@@ -49,8 +52,6 @@ for t in range(tests):
     inputs = np.count_nonzero(array[0,:,:] == b'o')
     outputs = np.count_nonzero(array[d-1,:,:] == b'o')
     pipes = np.count_nonzero(array == b'*')
-
-    print(inputs, outputs, pipes)
 
     internal_connections = (inputs + outputs + pipes * 3) - inputs - outputs
     print('YES' if internal_connections % 2 == 0 else 'NO')
