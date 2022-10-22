5ab0f92bb44b996c1396db244bea1a159721ef97
AngaBlue <contact@anga.blue>
2022-10-22 11:27:35 +1100

    Pizza cutter start

---
+++ python/pizza-cutter.py
@@ -0,0 +1,41 @@
+from typing import Set
+
+# Boilerplate parsing
+def parser():
+    while 1:
+        try:
+            data = list(input().split(' '))
+            for number in data:
+                if len(number) > 0:
+                    yield(number)   
+        except EOFError:
+            return
+
+input_parser = parser()
+
+def get_word():
+    global input_parser
+    return next(input_parser)
+
+def get_number():
+    data = get_word()
+    try:
+        return int(data)
+    except ValueError:
+        return float(data)
+
+# Get number of tests and loop
+tests = get_number()
+print('Tests: ', tests)
+for test in range(tests):
+    # Sets won't allow duplicates
+    values: Set[int] = {}
+
+    # Get number of values in test case
+    test_length = get_number()
+    for test in range(tests):
+        angle = get_number()
+        values.append(angle % 180 + 180)
+
+    # Number of slices = number of unique angles * 2
+    print(len(values) * 2)
