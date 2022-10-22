86e797f1c29cba27c7294c280ba631076bc2cf7f
AngaBlue <contact@anga.blue>
2022-10-22 16:59:55 +1100

    Palindrome cutting

---
+++ python/palindrome-cutting.py
@@ -0,0 +1,26 @@
+# Boilerplate parsing
+def parser():
+    while 1:
+        try:
+            data = list(input().split(' '))
+            for number in data:
+                if len(number) > 0:
+                    yield(number)   
+        except EOFError:
+            return
+
+input_parser = parser()
+
+def get_word():
+    global input_parser
+    return next(input_parser)
+
+def get_number():
+    data = get_word()
+    try:
+        return int(data)
+    except ValueError:
+        return float(data)
+
+N = get_number()
+M = get_number()
